d92edc9cbe8b63f36b7363c1bcb2d793757447d1
Türker Karapetti <trkrkrl53@gmail.com>
2022-04-15 10:27:27 +0300

    2048 tmm

--- jsprojects/2048-clone/app.js
+++ jsprojects/2048-clone/app.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded',()=>{
     const width=4
     let squares=[]
 
+    let score=0
+
     //create  a playing board
 
     function createBoard(){
@@ -33,6 +35,8 @@ document.addEventListener('DOMContentLoaded',()=>{
          if(squares[randomNumber].innerHTML==0){
 
             squares[randomNumber].innerHTML=2
+
+            checkForGameOver()
 
          }else generate()
 
@@ -121,9 +125,13 @@ document.addEventListener('DOMContentLoaded',()=>{
                 //bunu doğru yapmak için de key right içerisinde bir daha sağa alıyor
                 squares[i+1].innerHTML=0
 
+                score+=combinedTotal
+                scoreDisplay.innerHTML=score
+
             }
 
         }
+        checkForWin()
     }
 
     //tuşu dinlesin
@@ -135,8 +143,12 @@ document.addEventListener('DOMContentLoaded',()=>{
 
             keyRight()
 
-        }else if(e.keyCode==37){
+        }else if(e.keyCode===37){
             keyLeft()
+        }else if(e.keyCode===38){
+            keyUp()
+        }else if(e.keyCode===40){
+            keyDown()
         }
     }
 
@@ -153,12 +165,152 @@ document.addEventListener('DOMContentLoaded',()=>{
     function keyLeft(){
         moveLeft()
         combineRow()
-       // moveLeft()--buna gerek yok çünkü move left mümkün olduğunca sola kaydırırn geri kalan kısımda satırda sıfır koyar
+        moveLeft()//--bu gereksiz değil-çümkü 2 sayı birleşebiliir ama yanında bir tane sayıvarsa diğerleri birleşince onun da kayması gerek
         generate()
     }
 
     //yukarı ve aşağı kaldı
 
+    //aşağı
+    function moveDown(){
+        for (let i = 0; i < 4; i++) {
+
+            let totalOne=squares[i].innerHTML
+            let totalTwo=squares[i+(width)].innerHTML
+            let totalThree=squares[i+(width*2)].innerHTML
+            let totalFour=squares[i+(width*3)].innerHTML
+
+            let column=[parseInt(totalOne),parseInt(totalTwo),parseInt(totalThree),parseInt(totalFour)]
+
+            let filteredColumn=column.filter(num=>num)
+            let missing =4- filteredColumn.length
+            let zeros=Array(missing).fill(0)
+            let newColumn=zeros.concat(filteredColumn)
+
+            squares[i].innerHTML=newColumn[0]
+            squares[i+width].innerHTML=newColumn[1]
+            squares[i+(width*2)].innerHTML=newColumn[2]
+            squares[i+(width*3)].innerHTML=newColumn[3]
+
+
+
+            
+        }
+    }
+
+    //yukarı
+    function moveUp(){
+        for (let i = 0; i < 4; i++) {
+
+            let totalOne=squares[i].innerHTML
+            let totalTwo=squares[i+(width)].innerHTML
+            let totalThree=squares[i+(width*2)].innerHTML
+            let totalFour=squares[i+(width*3)].innerHTML
+
+            let column=[parseInt(totalOne),parseInt(totalTwo),parseInt(totalThree),parseInt(totalFour)]
+
+            let filteredColumn=column.filter(num=>num)
+            let missing =4- filteredColumn.length
+            let zeros=Array(missing).fill(0)
+            let newColumn=filteredColumn.concat(zeros)
+
+            squares[i].innerHTML=newColumn[0]
+            squares[i+width].innerHTML=newColumn[1]
+            squares[i+(width*2)].innerHTML=newColumn[2]
+            squares[i+(width*3)].innerHTML=newColumn[3]
+
+
+
+            
+        }
+    }
+    //combined column- combined row un muadili olacak
+    function combineColumn(){
+
+        for(let i=0;i<12;i++){
+
+            if(squares[i].innerHTML===squares[i+width].innerHTML){
+
+                let combinedTotal=parseInt(squares[i].innerHTML)+parseInt(squares[i+width].innerHTML)
+                squares[i].innerHTML=combinedTotal
+                squares[i+width].innerHTML=0
+
+                
+                score+=combinedTotal
+                scoreDisplay.innerHTML=score
+
+            }
+
+        }
+        checkForWin()
+        
+    }
+    //keyup ve keydown fonksiyonları--bunu yazdıktan sonra control methoduna git
+
+     function keyUp(){
+         moveUp()
+         combineColumn()
+         moveUp()
+         generate()
+
+     }
+     function keyDown(){
+         moveDown()
+         combineColumn()
+         moveDown()
+         generate()
+     }
+
+// check for the number 2048-combine icerisinde check yaparız
+function checkForWin(){
+    for(let i=0;i<squares.length;i++){
+        if(squares[i].innerHTML==2048){
+            resultDisplay.innerHTML='You Won'
+            document.removeEventListener('keyup',control)
+        }
+    }
+}
+//game over check-bunu generate içerisinde check edeceğiz
+function checkForGameOver() {
+    let zeros = 0
+    for (let i=0; i < squares.length; i++) {
+      if (squares[i].innerHTML == 0) {
+        zeros++
+      }
+    }
+    if (zeros === 0) {
+      resultDisplay.innerHTML = 'You LOSE'
+      document.removeEventListener('keyup', control)
+      setTimeout(() => clear(), 3000)
+    }
+  }
+  //zamanlayıcı ile bir süre  sonra sıfırlamış oluruz
+  
+  //clear timer
+  function clear() {
+    clearInterval(myTimer)
+  }
+
+  //kutucuktaki sayıya göre renklendirme
+  function addColours() {
+    for (let i=0; i < squares.length; i++) {
+      if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = '#afa192'
+      else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = '#eee4da'
+      else if (squares[i].innerHTML  == 4) squares[i].style.backgroundColor = '#ede0c8' 
+      else if (squares[i].innerHTML  == 8) squares[i].style.backgroundColor = '#f2b179' 
+      else if (squares[i].innerHTML  == 16) squares[i].style.backgroundColor = '#ffcea4' 
+      else if (squares[i].innerHTML  == 32) squares[i].style.backgroundColor = '#e8c064' 
+      else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = '#ffab6e' 
+      else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = '#fd9982' 
+      else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = '#ead79c' 
+      else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = '#76daff' 
+      else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = '#beeaa5' 
+      else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = '#d7d4f0' 
+    }
+}
+addColours()
+
+var myTimer = setInterval(addColours, 50)
 
 
 
